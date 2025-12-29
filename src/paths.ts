0158ad957e210f51986d0dca8de7d63e045464a1
schanver <mehmet-fatih.sanver@tu-dortmund.de>
2025-12-29 15:05:29 +0100

    keep the position of .env constant

--- src/paths.ts
+++ src/paths.ts
@@ -1,3 +1,13 @@
 import path from 'path';
+import dotenv from 'dotenv';
+import { fileURLToPath  } from 'url';
+
+// Get the current file's directory (which is ./src)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export const PROJECT_ROOT = path.resolve(__dirname, '..');
+
+
+// Move up one level to reach the project root
+dotenv.config({ path: path.resolve(PROJECT_ROOT, './.env') });
